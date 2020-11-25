f6d2a4bfc76f1bb7c7205e483a3cf814c925ffea
Moritz Schütz <moritz-schuetz@gmx.de>
2020-11-25 21:41:15 +0100

    GUI-Address added

--- Assets/NetworkingUI.cs
+++ Assets/NetworkingUI.cs
@@ -2,19 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using MLAPI;
+using MLAPI.Transports.UNET;
 
 public class NetworkingUI : MonoBehaviour
 {
     public string adress = "127.0.0.1";
-    public int port = 7777;
+    public string port = "7777";
     void OnGUI()
-    {        
-        if (GUI.Button(new Rect(10, 10, 150, 25), "Host Server"))
+    {
+        adress = GUI.TextField(new Rect(10, 70, 125, 25), adress);
+        port = GUI.TextField(new Rect(135, 70, 75, 25), port);
+        if (GUI.Button(new Rect(10, 10, 200, 25), "Host Server"))
         {
             NetworkingManager.Singleton.StartHost();
         }
-        if (GUI.Button(new Rect(10, 40, 150, 25), "Join Server"))
+        if (GUI.Button(new Rect(10, 40, 200, 25), "Join Server"))
         {
+            NetworkingManager.Singleton.GetComponent<UnetTransport>().ConnectAddress = adress;
+            NetworkingManager.Singleton.GetComponent<UnetTransport>().ConnectPort = int.Parse(port);
             NetworkingManager.Singleton.StartClient();
         }
 
