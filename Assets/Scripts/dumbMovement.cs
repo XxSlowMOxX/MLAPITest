aabfd5af5e402f0f11109e124db461d87f9b2b3c
Moritz Schütz <moritz-schuetz@gmx.de>
2020-11-28 20:53:50 +0100

    Movement Test

---
+++ Assets/Scripts/dumbMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+
+public class dumbMovement : NetworkedBehaviour
+{
+    void Update()
+    {
+        if (NetworkedObject.IsLocalPlayer)
+        {
+            this.transform.position += new Vector3(0,Input.GetAxis("Vertical")) * 0.05f;
+            this.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0) * 0.05f;
+        }
+    }
+}
