4f6351179815d40c68f224a90d2c86ccecd2e340
SlowMO_Desktop <moritz-schuetz@gmx.de>
2020-12-26 04:09:01 +0100

    Placement Preview

---
+++ Assets/Scripts/PlacementTest.cs
@@ -0,0 +1,26 @@
+﻿using MLAPI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlacementTest : NetworkedBehaviour
+{
+    public bool place = true;
+    public int gridSize = 2;
+    private Vector3 posMod;
+    void Update()
+    {
+        if(NetworkedObject.IsLocalPlayer && place)
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            posMod = new Vector3(Snapping.Snap(pos.x, gridSize), Snapping.Snap(pos.y, gridSize));
+            Debug.Log(posMod);
+        }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(posMod, 0.1f);
+    }
+}
