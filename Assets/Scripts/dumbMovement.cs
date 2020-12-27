13f4f481a3b8c873b3d665d7382c228d4d1ceab1
Moritz Schütz <moritz-schuetz@gmx.de>
2020-12-27 06:29:39 +0100

    Dynamic Camera

--- Assets/Scripts/dumbMovement.cs
+++ Assets/Scripts/dumbMovement.cs
@@ -7,17 +7,22 @@ using MLAPI.Connection;
 public class dumbMovement : NetworkedBehaviour
 {
     private void Start()
-    {
-        if (NetworkedObject.IsLocalPlayer)
-        {
+    {        
+        if (NetworkedObject.IsOwner)
+        {    
             DestroyImmediate(Camera.main.gameObject);
+            GameObject cam = new GameObject();
+            cam.name = "PlayerCamera";
+            cam.transform.parent = this.transform;
+            cam.transform.localPosition = new Vector3(0, 0, -10);
+            cam.AddComponent<Camera>().orthographic = true;
         }
     }
     void Update()
     {
         if (NetworkedObject.IsLocalPlayer)
         {
-            this.transform.position += new Vector3(0,Input.GetAxis("Vertical")) * 0.05f;
+            this.transform.position += new Vector3(0, Input.GetAxis("Vertical")) * 0.05f;
             this.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0) * 0.05f;
             //this.GetComponent<Trac>
         }
